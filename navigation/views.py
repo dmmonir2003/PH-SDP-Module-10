903fbd39c838495faabcb6c12e9f0309c029ce75
dmmonir2003 <dmmonir2003@gmail.com>
2023-11-29 13:42:21 +0600

    implement for and conditional statement in django use context

--- navigation/views.py
+++ navigation/views.py
@@ -5,7 +5,23 @@ from django.shortcuts import render
 
 def home(request):
 
-    d={'name':'monir','age':12,'marks':[60,40,19,40]}
+    d={'name':'monir','age':1,'marks':[60,40,19,40],'course':[
+        {
+            "id":1,
+            "name":"python",
+            "fee":5000
+        },
+        {
+            "id":2,
+            "name":"C++",
+            "fee":1000
+        },
+        {
+            "id":1,
+            "name":"django",
+            "fee":5600
+        },
+    ]}
 
     return render(request,'navigation/home.html',d)
 def about(request):
